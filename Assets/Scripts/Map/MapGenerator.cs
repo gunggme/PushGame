eaf1fabb25f3b0a0d32266263807af0d816d9a8e
gunggme <ekfrifajfl2019@gmail.com>
2023-01-10 21:56:07 +0900

    스크립트

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -11,7 +11,8 @@ public class MapGenerator : MonoBehaviour
     public Color ColorEdgeWall;
     public Color ColorCoinFloor;
     public Color ColorEnermyFloor;
-    public Color ColorExitFloor;
+    public Color ColorKeyFloor;
+    public Color ColorPlayerFloor;
     // 적 등장 위치
     public Color ColorResponse = new Color(64, 128, 128);
 
@@ -24,7 +25,9 @@ public class MapGenerator : MonoBehaviour
     public GameObject Wall;
     public GameObject CurveWall;
     public GameObject EdgeWall;
-    public GameObject Floor_Response;
+    public GameObject Enermy_Floor;
+    public GameObject Key_Floor;
+    public GameObject Player_Floor;
 
 
 
@@ -50,38 +53,57 @@ public class MapGenerator : MonoBehaviour
             for (int j = 0; j < mapWidth; j++)
             {
                 Color pixelColor = pixels[i * mapHeight + j];       // 0
-                Debug.Log(pixelColor);
+                //Debug.Log(pixelColor);
                 // 바닥
-                if (pixelColor == Color.white)
+                if (pixelColor == ColorFloor)
                 {
                     GameObject floor = GameObject.Instantiate(Floor, Terrain);
                     floor.transform.position = new Vector3(j * tileSize, 0, i * tileSize);
                 }
                 // 벽
-                if (pixelColor == Color.red)
+                if (pixelColor == ColorWal)
                 {
+                    Debug.Log("ㅁㄴㅇㄹ");
                     GameObject wall = GameObject.Instantiate(Wall, Terrain);
                     wall.transform.position = new Vector3(j * tileSize, 0, i * tileSize);
                     wall.transform.Rotate(new Vector3(0, GetWallRot(pixels, i, j), 0), Space.Self);
                 }
                 // 커브 벽
-                if (pixelColor == Color.green)
+                if (pixelColor == ColorCurveWall)
                 {
                     GameObject curveWall = GameObject.Instantiate(CurveWall, Terrain);
                     curveWall.transform.position = new Vector3(j * tileSize, 0, i * tileSize);
                     curveWall.transform.Rotate(new Vector3(0, GetCurveWallRot(pixels, i, j), 0), Space.Self);
                 }
                 // 모서리 벽
-                if (pixelColor == Color.blue)
+                if (pixelColor == ColorEdgeWall)
                 {
                     GameObject edgeWall = GameObject.Instantiate(EdgeWall, Terrain);
                     edgeWall.transform.position = new Vector3(j * tileSize, 0, i * tileSize);
                     edgeWall.transform.Rotate(new Vector3(0, GetEdgeWall(pixels, i, j), 0), Space.Self);
                 }
                 // 몬스터 생성 위치
-                if (pixelColor == ColorResponse)
+                if (pixelColor == ColorEnermyFloor)
                 {
-                    GameObject floor = GameObject.Instantiate(Floor_Response, Terrain);
+                    GameObject floor = GameObject.Instantiate(Enermy_Floor, Terrain);
+                    floor.transform.position = new Vector3(j * tileSize, 0, i * tileSize);
+                }
+                //열쇠 생성 위치
+                if(pixelColor == ColorKeyFloor)
+                {
+                    GameObject floor = GameObject.Instantiate(Key_Floor, Terrain);
+                    floor.transform.position = new Vector3(j * tileSize, 0, i * tileSize);
+                }
+                //플레이어 생성 위치
+                if (pixelColor == ColorPlayerFloor)
+                {
+                    GameObject floor = GameObject.Instantiate(Player_Floor, Terrain);
+                    floor.transform.position = new Vector3(j * tileSize, 0, i * tileSize);
+                }
+                //동전 생성 위치
+                if (pixelColor == ColorPlayerFloor)
+                {
+                    GameObject floor = GameObject.Instantiate(Player_Floor, Terrain);
                     floor.transform.position = new Vector3(j * tileSize, 0, i * tileSize);
                 }
 
@@ -95,17 +117,17 @@ public class MapGenerator : MonoBehaviour
         // 오른쪽
         float rot = 0f;
         // 아래
-        if (i - 1 >= 0 && (pixels[(i - 1) * mapHeight + j] == Color.black || pixels[(i - 1) * mapHeight + j] == Color.cyan))
+        if (i - 1 >= 0 && (pixels[(i - 1) * mapHeight + j] == Color.white || pixels[(i - 1) * mapHeight + j] == Color.cyan))
         {
             rot = 90f;
         }
         // 왼쪽
-        else if (j - 1 >= 0 && (pixels[i * mapHeight + (j - 1)] == Color.black || pixels[i * mapHeight + (j - 1)] == Color.cyan))
+        else if (j - 1 >= 0 && (pixels[i * mapHeight + (j - 1)] == Color.white || pixels[i * mapHeight + (j - 1)] == Color.cyan))
         {
             rot = 180f;
         }
         // 위
-        else if (i + 1 < mapHeight && (pixels[(i + 1) * mapHeight + j] == Color.black || pixels[(i + 1) * mapHeight + j] == Color.cyan))
+        else if (i + 1 < mapHeight && (pixels[(i + 1) * mapHeight + j] == Color.white || pixels[(i + 1) * mapHeight + j] == Color.cyan))
         {
             rot = -90f;
         }
@@ -117,17 +139,17 @@ public class MapGenerator : MonoBehaviour
         // 오른쪽 위
         float rot = 0f;
         // 오른쪽 아래
-        if (((pixels[i * mapHeight + j - 1] == Color.black || pixels[i * mapHeight + j - 1] == Color.cyan)) && ((pixels[(i - 1) * mapHeight + j] == Color.black) || (pixels[(i - 1) * mapHeight + j] == Color.cyan)))
+        if (((pixels[i * mapHeight + j - 1] == Color.white || pixels[i * mapHeight + j - 1] == Color.cyan)) && ((pixels[(i - 1) * mapHeight + j] == Color.white) || (pixels[(i - 1) * mapHeight + j] == Color.cyan)))
         {
             rot = 180f;
         }
         // 왼쪽 위
-        if (((pixels[i * mapHeight + j - 1] == Color.black) || (pixels[i * mapHeight + j - 1] == Color.cyan)) && ((pixels[(i + 1) * mapHeight + j] == Color.black) || (pixels[(i + 1) * mapHeight + j] == Color.cyan)))
+        if (((pixels[i * mapHeight + j - 1] == Color.white) || (pixels[i * mapHeight + j - 1] == Color.cyan)) && ((pixels[(i + 1) * mapHeight + j] == Color.white) || (pixels[(i + 1) * mapHeight + j] == Color.cyan)))
         {
             rot = -90f;
         }
         // 왼쪽 아래
-        if (((pixels[i * mapHeight + j + 1] == Color.black) || (pixels[i * mapHeight + j + 1] == Color.cyan)) && ((pixels[(i - 1) * mapHeight + j] == Color.black) || (pixels[(i - 1) * mapHeight + j] == Color.cyan)))
+        if (((pixels[i * mapHeight + j + 1] == Color.white) || (pixels[i * mapHeight + j + 1] == Color.cyan)) && ((pixels[(i - 1) * mapHeight + j] == Color.white) || (pixels[(i - 1) * mapHeight + j] == Color.cyan)))
         {
             rot = 90f;
         }
@@ -139,17 +161,17 @@ public class MapGenerator : MonoBehaviour
         // 오른쪽 위
         float rot = 0f;
         // 오른쪽 아래
-        if (i - 1 >= 0 && j + 1 < mapWidth && (pixels[(i - 1) * mapHeight + (j + 1)] == Color.black || pixels[(i - 1) * mapHeight + (j + 1)] == Color.cyan))
+        if (i - 1 >= 0 && j + 1 < mapWidth && (pixels[(i - 1) * mapHeight + (j + 1)] == Color.white || pixels[(i - 1) * mapHeight + (j + 1)] == Color.cyan))
         {
             rot = 90f;
         }
         // 왼쪽 위
-        else if (i - 1 >= 0 && j - 1 >= 0 && (pixels[(i - 1) * mapHeight + (j - 1)] == Color.black || pixels[(i - 1) * mapHeight + (j - 1)] == Color.cyan))
+        else if (i - 1 >= 0 && j - 1 >= 0 && (pixels[(i - 1) * mapHeight + (j - 1)] == Color.white || pixels[(i - 1) * mapHeight + (j - 1)] == Color.cyan))
         {
             rot = 180f;
         }
         // 왼쪽 아래
-        else if (i + 1 < mapHeight && j - 1 >= 0 && (pixels[(i + 1) * mapHeight + (j - 1)] == Color.black || pixels[(i + 1) * mapHeight + (j - 1)] == Color.cyan))
+        else if (i + 1 < mapHeight && j - 1 >= 0 && (pixels[(i + 1) * mapHeight + (j - 1)] == Color.white || pixels[(i + 1) * mapHeight + (j - 1)] == Color.cyan))
         {
             rot = -90f;
         }
